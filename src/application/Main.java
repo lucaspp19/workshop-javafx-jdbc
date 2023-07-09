b9111445ff77a4321a1218456029c34af500102b
lucaspp19 <lucaspp191@gmail.com>
2023-07-09 19:01:52 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+	private static Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -21,16 +22,25 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
+	
+
+
 }
--- src/gui/MainWiewController.java
+++ src/gui/MainWiewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainWiewController implements Initializable {
 
@@ -35,12 +44,36 @@ public class MainWiewController implements Initializable {
 	@FXML
 	public void OnMenuItemAboutAction() {
 
-		System.out.println("OnMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 
 	}
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+
+	}
+
+	private synchronized void loadView(String absoluteName) {
+
+		
+
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainBox.getChildren().get(0);
+			mainBox.getChildren().clear();
+			mainBox.getChildren().add(mainMenu);
+			mainBox.getChildren().addAll(newBox.getChildren());
+			
+
+		} catch (IOException e) {
+
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 
 	}
 
