46cfd98c37d76c37142d55a6020049f73cbe18a6
lucaspp19 <lucaspp191@gmail.com>
2023-07-16 11:18:03 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,9 +45,10 @@ public class DepartmentListController implements Initializable {
 	private ObservableList<Department> obsList;
 
 	@FXML
-	public void onBtNewAction() {
+	public void onBtNewAction(ActionEvent event) {
 
-		System.out.println("onBtNewAction");
+		Stage parentStage = Utils.currentSatge(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 
 	}
 
@@ -62,17 +72,41 @@ public class DepartmentListController implements Initializable {
 		tableWiewDepartment.prefHeightProperty().bind(stage.heightProperty());
 
 	}
-	
+
 	public void updateTableView() {
-		
-		if(service == null) {
-			
+
+		if (service == null) {
+
 			throw new IllegalStateException("Service was null");
 		}
-		
+
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableWiewDepartment.setItems(obsList);
+	}
+
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+
+		try {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department date");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+				
+
+		} catch (IOException e) {
+
+			Alerts.showAlert("IO Exception", "Erro load view", e.getMessage(), AlertType.ERROR);
+
+		}
+
 	}
 
 }
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,5 +1,15 @@
 package gui.util;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class Utils {
+	
+ public static Stage currentSatge(ActionEvent event) {
+	 
+	 return (Stage)((Node)event.getSource()).getScene().getWindow();
+	 
+ }
 
 }
